e841112744e7fb5c26f5ee1d0576e474d45eb2c1
muhammed123939 <mohammed123939@miuegypt.edu.eg>
2024-09-07 02:34:17 +0300

    End of section 2

---
+++ api/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using System;
+using api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+ 
+}
+ 
